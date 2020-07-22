fc6a95bf526a877bf224c9c4503e79ee74adcd56
afashio <push.webstudio@gmail.com>
2020-07-22 17:16:23 +0300

    Fixed migration

--- src/migrations/m160323_111940_insert_new_seo_data.php
+++ src/migrations/m160323_111940_insert_new_seo_data.php
@@ -32,12 +32,6 @@ class m160323_111940_insert_new_seo_data extends \yii\db\Migration
                     1,
                     0,
                 ],
-                [
-                    \notgosu\yii2\modules\metaTag\models\MetaTag::META_SEO_TEXT,
-                    'Seo Text',
-                    1,
-                    0,
-                ],
             ]
         );
     }
